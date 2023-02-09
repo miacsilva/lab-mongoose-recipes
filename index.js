d27a6e12df1a14380ac366c8267f698c5b0cb656
miacsilva <miacsilvac@hotmail.com>
2023-02-09 16:15:52 +0000

    lab solved

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 //Method 1 : Using Async Await
+
+const bolonhesa = {title: "Bolonhesa", level:"Easy Peasy", ingredients: ["pasta", "tomatoes", "meat"], cuisine:"Idk", dishType: "main_course", creator:"Moi Je"}
+
 
 const manageRecipes = async () => {
   try {
@@ -18,7 +21,15 @@ const manageRecipes = async () => {
     // Before adding any recipes to the database, let's remove all existing ones
     await Recipe.deleteMany();
 
-    // Run your code here, after you have insured that the connection was made
+    await Recipe.create(bolonhesa);
+
+    await Recipe.insertMany(data)
+    
+    await Recipe.updateOne({title:"Rigatoni alla Genovese"}, {duration:100})
+    
+    await Recipe.deleteOne({title:"Carrot Cake"})
+
+    mongoose.disconnect();
   } catch (error) {
     console.log(error);
   }
